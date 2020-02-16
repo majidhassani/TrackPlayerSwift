c4020cedc6e7c8dd56a97f755c811e397e09c6cf
sun <majid.hassani23@gmail.com>
2020-02-16 14:29:13 +0330

    Initial Commit

---
+++ TrackPlayerSwift/TrackPlayerSwift.h
@@ -0,0 +1,19 @@
+//
+//  TrackPlayerSwift.h
+//  TrackPlayerSwift
+//
+//  Created by sun on 2/16/20.
+//  Copyright © 2020 hamrahafzaryektatechnology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TrackPlayerSwift.
+FOUNDATION_EXPORT double TrackPlayerSwiftVersionNumber;
+
+//! Project version string for TrackPlayerSwift.
+FOUNDATION_EXPORT const unsigned char TrackPlayerSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrackPlayerSwift/PublicHeader.h>
+
+
