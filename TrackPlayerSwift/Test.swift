6bb3edb118653d27cef9e85ee009fa68e109a5f1
sun <majid.hassani23@gmail.com>
2020-02-16 14:55:23 +0330

    first commit

---
+++ TrackPlayerSwift/Test.swift
@@ -0,0 +1,9 @@
+//
+//  Test.swift
+//  TrackPlayerSwift
+//
+//  Created by sun on 2/16/20.
+//  Copyright © 2020 hamrahafzaryektatechnology. All rights reserved.
+//
+
+import Foundation
---
+++ TrackPlayerSwiftTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackPlayerSwiftTest
+//
+//  Created by sun on 2/16/20.
+//  Copyright © 2020 hamrahafzaryektatechnology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
